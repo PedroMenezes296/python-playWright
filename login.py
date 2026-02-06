085efd5a7c72c856f79b411a280edffb8e5bcb3b
PedroMenezes296 <barananomenezespedro@gmail.com>
2026-02-06 09:16:34 -0300

    login implementation botoes

--- login.py
+++ login.py
@@ -2,7 +2,8 @@
 # Pré-requisito: Chrome já aberto com CDP em http://localhost:9222
 # Ex.: chrome.exe --remote-debugging-port=9222 --user-data-dir="C:\playwright-profile-youtube" --profile-directory="Default"
 
-from playwright.sync_api import sync_playwright
+from playwright.sync_api import sync_playwright,expect
+
 
 VIDEO_URL = "https://www.youtube.com/watch?v=-vjW4dHEvCs&list=OLAK5uy_kCgEwOlrnK0sUf-U4jfnw9XAM3XKvyU_k"
 
@@ -13,6 +14,21 @@ def pegar_contexto_e_pagina(browser):
     pagina.bring_to_front()
     return contexto, pagina
 
+def esperar_saiba_ou_veja(pagina, saiba_locator, veja_locator, acesseSite_locator, intervalo_ms=200):
+    while True:
+        if saiba_locator.is_visible():
+            return "saiba"
+        if veja_locator.is_visible():
+            return "veja"
+        if acesseSite_locator.is_visible():
+            return "acessar_site"
+        pagina.wait_for_timeout(intervalo_ms)
+
+def clicar_no_botao_pular(pular_locator):
+    expect(pular_locator).to_be_visible()
+    pular_locator.click().timeout(0)
+    
+    
 with sync_playwright() as pw:
     # Conecta no Chrome já aberto com CDP
     browser = pw.chromium.connect_over_cdp("http://localhost:9222")
@@ -26,6 +42,38 @@ with sync_playwright() as pw:
     print("URL após goto:", pagina.url)
 
     input("Vídeo aberto. Pressione ENTER para encerrar a conexão...")
+    
+    botao_vejamais_anuncio = pagina.get_by_label("Veja mais", exact=True)
+    botao_saibaMais_anuncio = pagina.get_by_label("Saiba mais", exact=True)
+    botao_acessar_site_anuncio = pagina.get_by_role("link", name="Acessar o site This link")
+    botao_pular_youtube = pagina.get_by_role("button", name="Pular", exact=True)
+
+    qual = esperar_saiba_ou_veja(pagina, botao_saibaMais_anuncio, botao_vejamais_anuncio, botao_acessar_site_anuncio)
+
+    if qual == "saiba":
+    # clica no saiba mais
+        with contexto.expect_page() as pagina2_info:
+            botao_saibaMais_anuncio.click()
+        pagina2 = pagina2_info.value
+        pagina2.close()
+        pagina.locator("#movie_player video").click()  # volta o foco para o video
+        clicar_no_botao_pular(botao_pular_youtube)
+    elif qual == "acessar_site":
+        with contexto.expect_page() as pagina4_info:
+            botao_acessar_site_anuncio.click()
+        pagina4 = pagina4_info.value
+        pagina4.close()
+        pagina.locator("#movie_player video").click()  # volta o foco para o video
+        clicar_no_botao_pular(botao_pular_youtube)
+    else:
+    # clica no veja mais
+        with contexto.expect_page() as pagina3_info:
+            botao_vejamais_anuncio.click()
+        pagina3 = pagina3_info.value
+        pagina3.close()
+        pagina.locator("#movie_player video").click()  # volta o foco para o video
+        clicar_no_botao_pular(botao_pular_youtube)
+
 
     # Fecha a conexão CDP (pode fechar o Chrome dependendo do caso)
     browser.close()
